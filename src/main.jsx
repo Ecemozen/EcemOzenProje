22fe630d69559de9adb65338ba3e95e20457b88d
Ecemozen <ecemozen1502@gmail.com>
2026-06-20 01:48:55 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css' // İşte bu sihirli satır eksikti, bunu ekledik!
+import './index.css' 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
